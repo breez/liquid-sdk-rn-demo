4715736160a3e9610dad1cd17b8be29fe33320f3
yse <hydra_yse@proton.me>
2025-01-11 02:22:14 +0100

    feat: add `WalletInfo`

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,29 @@
 import { Text, View } from "react-native";
 
+import { useEffect } from "react";
+
+import WalletInfo from "@/components/WalletInfo";
+import { Link, useNavigation } from "expo-router";
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import { getInfo } from "@breeztech/react-native-breez-sdk-liquid";
+import { setInfo } from "@/store/reducers/info";
 export default function Index() {
+  const navigation = useNavigation();
+
+  const dispatch = useAppDispatch();
+  const info = useAppSelector((store) => store.info);
+
+  useEffect(() => {
+    navigation.setOptions({
+      title: 'Breez Nodeless SDK RN Demo'
+    })
+  }, [navigation])
+
+  useEffect(() => {
+    getInfo()
+      .then((info) => dispatch(setInfo(info)))
+  }, [])
+
   return (
     <View
       style={{
@@ -9,7 +32,12 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      {info ? (
+        <>
+          <WalletInfo info={info.walletInfo} />
+        </>
+      ) : <Text>Loading wallet information...</Text>
+      }
     </View>
   );
 }
